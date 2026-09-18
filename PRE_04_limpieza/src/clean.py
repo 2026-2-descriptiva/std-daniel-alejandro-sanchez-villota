5687fe20b5b587c267b2ed7cf703b637779fd2b8
Daniel Sanchez <dasanchezvi@unal.edu.co>
2026-09-18 18:31:41 -0500

    Clean data Donee

--- PRE_04_limpieza/src/clean.py
+++ PRE_04_limpieza/src/clean.py
@@ -1,5 +1,5 @@
 import os
-	
+
 import pandas as pd
 
 INPUT_FILE = "PRE_04_limpieza/data/ventas.csv"
@@ -69,6 +69,27 @@ SUPPLIER_REPLACEMENTS = {
 }
 
 
+COUNTRY_REPLACEMENTS = {
+    "Colombia": [
+        "CO",
+        "COL",
+        "colombia",
+        "COLOMBIA",
+    ],
+}
+
+CITY_REPLACEMENTS = {
+    "Bogotá": [
+        "BOGOTÁ",
+        "bogotá",
+    ],
+    "Medellín": [
+        "MEDELLÍN",
+        "medellín",
+        "Medellin",
+    ]
+}
+
 
 def make_replacements(series, replacements):
     for replacement, values in replacements.items():
@@ -91,10 +112,52 @@ def clean_column_names(df):
     df.columns = replace_space_with_underscore(df.columns)
     return df
 
+def transform_dd_dd_dd_to_dd_dd_20dd(series):
+    series = series.str.replace(r"^(\d{2})-(\d{2})-(\d{2})$", r"\1-\2-20\3", regex=True)
+    return series
+
+def transform_dd_mm_yyyy_to_yyyy_mm_dd(series):
+    series = series.str.replace(r"^(\d{2})-(\d{2})-(\d{4})$", r"\3-\2-\1", regex=True)
+    return series
+
+def transform_yyyy_dd_mm_to_yyyy_mm_dd(series):
+
+    def f(date):
+        parts = date.split("-")
+        if int(parts[1]) > 12:
+            return f"{parts[0]}-{parts[2]}-{parts[1]}"
+        return date
+
+    series = series.apply(f)
+    return series
+
+# -----
 
 def clean_supplier_column(series):
     series = strip_whitespace(series)
     series = make_replacements(series, SUPPLIER_REPLACEMENTS)
+    return series
+
+def clean_country_column(series):
+    series = strip_whitespace(series)
+    series = make_replacements(series, COUNTRY_REPLACEMENTS)
+    return series
+
+def clean_city_column(series):
+    series = strip_whitespace(series)
+    series = make_replacements(series, CITY_REPLACEMENTS)
+    return series
+
+
+
+
+def clean_purchase_date_column(series):
+    series = strip_whitespace(series)
+    series = series.str.replace(r".", "-", regex=False)
+    series = series.str.replace(r"/", "-", regex=False)
+    series = transform_dd_dd_dd_to_dd_dd_20dd(series)
+    series = transform_dd_mm_yyyy_to_yyyy_mm_dd(series)
+    series = transform_yyyy_dd_mm_to_yyyy_mm_dd(series)
     return series
 
 def main():
@@ -104,6 +167,9 @@ def main():
     df = clean_column_names(df)
     
     df["supplier"] = clean_supplier_column(df["supplier"])
+    df["country"] = clean_country_column(df["country"])
+    df["city"] = clean_city_column(df["city"])
+    df["purchase_date"] = clean_purchase_date_column(df["purchase_date"])
     
     df.to_csv(OUTPUT_FILE, index=False)
 
@@ -111,9 +177,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
-    
-    
-    
-    
     
